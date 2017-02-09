a873c938d9d1b8de4ece5ea54169e527b7bf679d
Akhmad Fathonih <akhmadf@gmail.com>
2017-02-09 23:11:13 +0900

    support for Vue 2

--- vue.drag-and-drop.js
+++ vue.drag-and-drop.js
@@ -1,145 +1,195 @@
 /* globals Vue */
 (function () {
 
-  var DragAndDrop = {}
-
-  DragAndDrop.install = function (Vue) {
-    Vue.directive('drag-and-drop', {
-      params: [
-      'drag-and-drop',
-      'drag-start',
-      'drag',
-      'drag-over',
-      'drag-enter',
-      'drag-leave',
-      'drag-end',
-      'drop',
-      'draggable',
-      'droppable'
-      ],
-      bind: function () {
-        // use the VM so we only have 1 dragging item per app
-        this.vm._dragSrcEl = null;
-
-        if(this.params.draggable === undefined){
-          this.params.draggable = true;
-        }
-        if(this.params.droppable === undefined){
-          this.params.droppable = true;
-        }
-        // transfer "false" => false, "true" => true
-        var blooeanMaps = {
-          true: true,
-          false: false
-        }
-
-        var draggable = blooeanMaps[this.params.draggable];
-        var droppable = blooeanMaps[this.params.droppable];
-        var emptyFn = function(){};
-
-        this.handleDragStart = function (e) {
-          e.target.classList.add('dragging');
-          this.vm._dragSrcEl = e.target;
-          e.dataTransfer.effectAllowed = 'move';
-          // Need to set to something or else drag doesn't start
-          e.dataTransfer.setData('text', '*');
-          if (typeof(this.vm[this.params.dragStart]) === 'function') {
-            this.vm[this.params.dragStart].call(this, e.target, e);
-          }
-        }.bind(this);
-        this.handleDragOver = function(e) {
-          if (e.preventDefault) {
-            // allows dropping
-            e.preventDefault();
-          }
-          e.dataTransfer.dropEffect = 'move';
-          e.target.classList.add('drag-over');
-          if (typeof(this.vm[this.params.dragOver]) === 'function') {
-            this.vm[this.params.dragOver].call(this, e.target, e);
-          }
-          return false;
-        }.bind(this);
-        this.handleDragEnter = function(e) {
-          if (typeof(this.vm[this.params.dragEnter]) === 'function') {
-            this.vm[this.params.dragEnter].call(this, e.target, e);
-          }
-          e.target.classList.add('drag-enter');
-        }.bind(this);
-        this.handleDragLeave = function(e) {
-          if (typeof(this.vm[this.params.dragLeave]) === 'function') {
-            this.vm[this.params.dragLeave].call(this, e.target, e);
-          }
-          e.target.classList.remove('drag-enter');
-        }.bind(this);
-        this.handleDrag = function(e) {
-            if (typeof(this.vm[this.params.drag]) === 'function') {
-                this.vm[this.params.drag].call(this, e.target, e);
-            }
-        }.bind(this);
-        this.handleDragEnd = function(e) {
-          e.target.classList.remove('dragging', 'drag-over', 'drag-enter');
-          if (typeof(this.vm[this.params.dragEnd]) === 'function') {
-            this.vm[this.params.dragEnd].call(this, e.target, e);
-          }
-        }.bind(this);
-        this.handleDrop = function(e) {
-          e.preventDefault();
-          if (e.stopPropagation) {
-            // stops the browser from redirecting.
-            e.stopPropagation();
-          }
-          // Don't do anything if dropping the same column we're dragging.
-          if (this.vm._dragSrcEl != e.target) {
-            if (typeof(this.vm[this.params.drop]) === 'function') {
-              var el = (e.target.draggable || draggable) ? e.target : e.target.parentElement;
-              this.vm[this.params.drop].call(this, this.vm._dragSrcEl, el, e);
-            }
-          }
-          return false;
-        }.bind(this);
-
-        if(!draggable){
-          this.handleDragStart = emptyFn;
-          this.handleDragEnter = emptyFn;
-          this.handleDrag = emptyFn;
-          this.handleDragLeave = emptyFn;
-          this.handleDragEnd = emptyFn;
-        }
-        if(!droppable){
-          this.handleDrop = emptyFn;
-        }
-        // setup the listeners
-        draggable && this.el.setAttribute('draggable', 'true');
-        this.el.addEventListener('dragstart', this.handleDragStart, false);
-        this.el.addEventListener('dragenter', this.handleDragEnter, false);
-        this.el.addEventListener('dragover', this.handleDragOver, false);
-        this.el.addEventListener('drag', this.handleDrag, false);
-        this.el.addEventListener('dragleave', this.handleDragLeave, false);
-        this.el.addEventListener('dragend', this.handleDragEnd, false);
-        this.el.addEventListener('drop', this.handleDrop, false);
-      },
-      update: function (newValue, oldValue) {
-        // console.log(this);
-      },
-      unbind: function () {
-        // shut er' down
-        this.el.classList.remove('dragging', 'drag-over', 'drag-enter');
-        this.el.removeAttribute('draggable');
-        this.el.removeEventListener('dragstart', this.handleDragStart);
-        this.el.removeEventListener('dragenter', this.handleDragEnter);
-        this.el.removeEventListener('dragover', this.handleDragOver);
-        this.el.removeEventListener('dragleave', this.handleDragLeave);
-        this.el.removeEventListener('drag', this.handleDrag);
-      }
+    var DragAndDrop = {}
+    // singleton property
+    Object.defineProperty(DragAndDrop, 'vm', {
+        value: {},
+        writable: true,
+        enumerable: true,
+        configurable: true
     });
-  }
 
-  if (typeof exports == "object") {
-    module.exports = DragAndDrop
-  } else if (typeof define == "function" && define.amd) {
-    define([], function(){ return DragAndDrop })
-  } else if (window.Vue) {
-    window.DragAndDrop = DragAndDrop
-    Vue.use(DragAndDrop)
-  }
+    DragAndDrop.install = function (Vue) {
+        function findParent(fromEl, untilCallbackSatisfiedFunction) {
+            if (fromEl == document || fromEl.parentElement == document) {
+                return null
+            } else {
+                if (untilCallbackSatisfiedFunction(fromEl.parentElement)) {
+                    return fromEl.parentElement
+                } else {
+                    findParent(fromEl.parentElement)
+                }
+            }
+        }
+        //console.log('DragAndDrop', Vue)
+        Vue.directive('drag-and-drop', {
+            bind: function (el, binding) {
+                var paramSet = [
+                    'drag-and-drop',
+                    'drag-start',
+                    'drag',
+                    'drag-over',
+                    'drag-enter',
+                    'drag-leave',
+                    'drag-end',
+                    'drop',
+                    'draggable',
+                    'droppable'
+                ]
+
+                Object.defineProperty(el, 'vm', {
+                    value: {},
+                    writable: true,
+                    enumerable: true,
+                    configurable: true
+                });
+
+
+                // this element params
+                var params = {}
+                //for(i in paramSet) {params[i] = undefined}}
+
+                // use the VM so we only have 1 dragging item per app
+                //console.log('xbinding', this);
+                DragAndDrop.vm._dragSrcEl = null;
+
+                for (var passedParam in binding.value) {
+                    if (paramSet.indexOf(passedParam) >= 0) {
+                        var camelCase = passedParam.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); });
+                        params[camelCase] = binding.value[passedParam]
+                    }
+                }
+
+                if (params.draggable === undefined) {
+                    params.draggable = true;
+                }
+                if (params.droppable === undefined) {
+                    params.droppable = true;
+                }
+
+                //console.log('params', params)
+                // transfer "false" => false, "true" => true
+                var booleanMaps = {
+                    true: true,
+                    false: false
+                }
+
+                var draggable = booleanMaps[params.draggable];
+                var droppable = booleanMaps[params.droppable];
+                var emptyFn = function () {};
+
+                el.vm.handleDragStart = function (e) {
+                    e.target.classList.add('dragging');
+                    DragAndDrop.vm._dragSrcEl = e.target;
+                    //console.log('handleDragStart', DragAndDrop.vm)
+                    e.dataTransfer.effectAllowed = 'move';
+                    // Need to set to something or else drag doesn't start
+                    e.dataTransfer.setData('text', '*');
+                    //console.log(params.dragStart)
+                    if (typeof params.dragStart === 'function') {
+                        params.dragStart.call(this, e.target, e);
+                    }
+                }.bind(this);
+                el.vm.handleDragOver = function (e) {
+                    if (e.preventDefault) {
+                        // allows dropping
+                        e.preventDefault();
+                    }
+                    e.dataTransfer.dropEffect = 'move';
+                    e.target.classList.add('drag-over');
+                    if (typeof params.dragOver === 'function') {
+                        params.dragOver.call(this, e.target, e);
+                    }
+                    return false;
+                }.bind(this);
+                el.vm.handleDragEnter = function (e) {
+                    if (typeof params.dragEnter === 'function') {
+                        params.dragEnter.call(this, e.target, e);
+                    }
+                    e.target.classList.add('drag-enter');
+                }.bind(this);
+                el.vm.handleDragLeave = function (e) {
+                    if (typeof params.dragLeave === 'function') {
+                        params.dragLeave.call(this, e.target, e);
+                    }
+                    e.target.classList.remove('drag-enter');
+                }.bind(this);
+                el.vm.handleDrag = function (e) {
+                    if (typeof params.drag === 'function') {
+                        params.drag.call(this, e.target, e);
+                    }
+                }.bind(this);
+                el.vm.handleDragEnd = function (e) {
+                    e.target.classList.remove('dragging', 'drag-over', 'drag-enter');
+                    if (typeof params.dragEnd === 'function') {
+                        params.dragEnd.call(this, e.target, e);
+                    }
+                }.bind(this);
+
+                el.vm.handleDrop = function (e) {
+                    //console.log('handleDrop', e)
+                    e.preventDefault();
+                    if (e.stopPropagation) {
+                        // stops the browser from redirecting.
+                        e.stopPropagation();
+                    }
+                    // Don't do anything if dropping the same column we're dragging.
+                    if (DragAndDrop.vm._dragSrcEl != e.target) {
+                        if (typeof params.drop === 'function') {
+                            var targetElm = findParent(e.target, function (elm) { return elm.getAttribute('draggable') == 'true'});
+                            //console.log('targetElm', targetElm)
+                            params.drop.call(this, DragAndDrop.vm._dragSrcEl, targetElm, e);
+                        }
+                    }
+                    return false;
+                }.bind(this);
+
+                if (!draggable) {
+                    el.vm.handleDragStart = emptyFn;
+                    el.vm.handleDragEnter = emptyFn;
+                    el.vm.handleDrag = emptyFn;
+                    el.vm.handleDragLeave = emptyFn;
+                    el.vm.handleDragEnd = emptyFn;
+                }
+                if (!droppable) {
+                    el.vm.handleDrop = emptyFn;
+                }
+                // setup the listeners
+                draggable && el.setAttribute('draggable', 'true');
+                //console.log(el.vm)
+                el.addEventListener('dragstart', el.vm.handleDragStart, false);
+                el.addEventListener('dragenter', el.vm.handleDragEnter, false);
+                el.addEventListener('dragover', el.vm.handleDragOver, false);
+                el.addEventListener('drag', el.vm.handleDrag, false);
+                el.addEventListener('dragleave', el.vm.handleDragLeave, false);
+                el.addEventListener('dragend', el.vm.handleDragEnd, false);
+                el.addEventListener('drop', el.vm.handleDrop, false);
+            },
+            unbind: function (el) {
+                // shut er' down
+                el.classList.remove('dragging', 'drag-over', 'drag-enter');
+                el.removeAttribute('draggable');
+                el.removeEventListener('dragstart', el.vm.handleDragStart);
+                el.removeEventListener('dragenter', el.vm.handleDragEnter);
+                el.removeEventListener('dragover', el.vm.handleDragOver);
+                el.removeEventListener('dragleave', el.vm.handleDragLeave);
+                el.removeEventListener('drag', el.vm.handleDrag);
+            }
+        });
+    }
+
+    if (typeof exports == "object") {
+        module.exports = DragAndDrop
+        //console.log('exports')
+    } else if (typeof define == "function" && define.amd) {
+        //console.log('define')
+        define('vue-drag-and-drop', [], function () {
+            return DragAndDrop
+        })
+    } else if (window.Vue) {
+        //console.log('window.Vue')
+        window.DragAndDrop = DragAndDrop
+        Vue.use(DragAndDrop)
+    }
 })()
